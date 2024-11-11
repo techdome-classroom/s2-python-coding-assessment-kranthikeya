42b849322b31253db156cf4b0e4deb37c94f7cb2
kranthi kumar dande <139384937+kranthikeya@users.noreply.github.com>
2024-11-11 16:37:24 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,7 +4,28 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
 
+        total = 0
 
+        for i in range(len(s) - 1):
+            current_value = roman_map[s[i]]
+            next_value = roman_map[s[i + 1]]
+            
+            if current_value < next_value:
+                total -= current_value  # Subtraction case (e.g., IV, IX)
+            else:
+                total += current_value  # Addition case (e.g., VI, XI)
 
+        # Add the value of the last character
+        total += roman_map[s[-1]]
+
+        return total

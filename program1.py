fd4803c6f3185688a826ac46bed72d3e3a0ae581
kranthi kumar dande <139384937+kranthikeya@users.noreply.github.com>
2024-11-11 16:36:46 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,17 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        stack = []
+        bracket_map = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
 
+        for char in s:
+            if char in bracket_map:  # If it's a closing bracket
+                top_element = stack.pop() if stack else '#'  # Pop from stack or use dummy value
+                if bracket_map[char] != top_element:
+                    return False
+            else:  # If it's an opening bracket
+                stack.append(char)
 
-
-
-
-
-
-    
-
-
-
-  
-
+        return not stack  # If stack is empty, return True; otherwise, False
